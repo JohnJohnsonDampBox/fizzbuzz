8ae81aca28e5c2f61b516afef556312268d9394b
John Johnson <dampboxdev@gmail.com>
2014-01-21 13:37:33 -0800

    fizzbuzz special added

--- Fizzbuzz.rb
+++ Fizzbuzz.rb
@@ -1,16 +1,32 @@
 class Fizzbuzz
 
   def initialize(x)
-    @nums = x
-    @str = ''
-    @num.times do |n|
-      @str + 'Fizz' if n%3 == 0
-      @str + 'Buzz' if n%5 == 0
-      puts @str
+    (1..x).each do |n|
+      str = ''
+      str += 'Fizz' if n%3 == 0
+      str += 'Buzz' if n%5 == 0
+      puts(str.empty? ? n : str)
     end
   end
 
 end
 
-fb = Fizzbuzz(40)
-puts fb
+class FizzbuzzSpecial
+
+  def initialize(x, args={})
+    (1..x).each do |n|
+      str = ''
+      args.each do |k, v|
+        str += v if n%k == 0
+      end
+    puts(str.empty? ? n : str)
+    end
+  end
+end
+
+
+puts 'This is Fizzbuzz'
+f = Fizzbuzz.new(105)
+
+puts 'This is Fizzbuzz special'
+f = FizzbuzzSpecial.new(105,{ 3 => 'Fizz', 5 => 'Buzz', 7 => 'Siv'})
---
+++ README.md
@@ -0,0 +1,19 @@
+FIZZBUZZ
+========
+
+Takes in an integer and outputs a list of 0-integer
+with numbers evenly divisible by 3 replaced with 'Fizz'
+and numbers evenly divisible by 5 replaced with 'Buzz'
+with numbers divisible by both being replaced with 'FizzBuzz'.
+
+
+Fizzbuzz(x) takes one integer
+
+
+FIZZBUZZ SPECIAL
+================
+
+Takes in an integer and a hash of number, outputs.  While
+counting up to the integer it will output the value if the integer being iterated over is evenly divisible by the key.
+
+FizzbuzzSpecial(105,{ 3 => 'Fizz', 5 => 'Buzz', 7 => 'Siv'})
